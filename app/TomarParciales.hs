cf66a1e33351519bebd379fedfd1348806145d21
Octavio <octavio.el.magnifico@gmail.com>
2018-08-06 16:52:04 -0300

    Ahora compila.

--- app/TomarParciales.hs
+++ app/TomarParciales.hs
@@ -1,9 +1,10 @@
 import ProfesorRobot
 import CargarDatos
 
-TomarParciales = do
-  fecha <- readFile "fecha.txt"
+main :: IO()
+main = do
+  fechaISO <- readFile "fecha.txt"
   cuatrimestre <- readFile "MateriasDelCuatrimestre.untref"
   let materias = recuperarMaterias cuatrimestre
-       putStrLn( darOrden ( aRclone (isoAFecha entrada) materias) )
+  putStrLn( darOrden ( aRclone (isoAFecha fechaISO) materias) )
 
